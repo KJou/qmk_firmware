64a922d1818a04e0958c99e8e102733726fba3f5
Kevin Jou <44953361+KJou@users.noreply.github.com>
2025-03-20 15:32:58 -0700

    Adds GAME2 layer

--- keyboards/keebio/nyquist/keymaps/kjou/keymap.c
+++ keyboards/keebio/nyquist/keymaps/kjou/keymap.c
@@ -10,11 +10,13 @@
 #define _GAME1 2
 #define _LOWER 3
 #define _RAISE 4
+#define _GAME2 5
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   GAME1,
+  GAME2,
   LOWER,
   RAISE,
   ADJUST,
@@ -86,6 +88,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DOT,  KC_LCTL, KC_LALT, KC_LBRC, KC_RBRC, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
+/* Game2
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   \  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  ,   | Ctrl |  Alt |   .  |   /  |Space |Space |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+ [_GAME2] = LAYOUT( \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_BSLS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_COMM, KC_LCTL, KC_LALT, KC_DOT,  KC_SLSH, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+),
+
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -125,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD,    KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,  \
   KC_DEL,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______,  _______,  KC_UNDS, KC_PLUS, KC_PIPE, \
   _______,    KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______,  _______, _______, _______, \
-  TG(_GAME1), TG(_WKL), _______, _______, _______,  KC_ENT,  KC_ENT,  _______,  _______,  _______, _______, _______ \
+  TG(_GAME1), _______, _______, _______, _______,  KC_ENT,  KC_ENT,  _______,  _______,  _______, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
@@ -138,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |GAME1 |GAME2 |  WKL |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT( \
@@ -146,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_DEL, \
   _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______,  _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  TG(_GAME1), TG(_GAME2), TG(_WKL),, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 
 
@@ -222,6 +245,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
           rgblight_enable_noeeprom();
           rgblight_mode_noeeprom(2);
           rgblight_sethsv_noeeprom(HSV_MAGENTA);
+        }
+        break;
+    case _GAME2:
+        if (!(rgblight_is_enabled())) {
+          rgblight_enable_noeeprom();
+          rgblight_mode_noeeprom(2);
+          rgblight_sethsv_noeeprom(HSV_RED);
         }
         break;
     case _RAISE:
